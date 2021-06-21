5359b304a1c62d3ca0ea2b4208b1427a49978909
Alicia Ramirez <aramirez@rrc.ca>
2021-06-21 11:39:57 -0500

    initial commit, first page complete

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  // basePath: '/caracol',
+  // assetPrefix: '/caracol',
+  images: {
+    loader: 'cloudinary',
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    path: 'https://res.cloudinary.com/dnliyglel/image/upload/',
+  },
+    exportPathMap: async function() {
+    const paths = {
+      '/': { page: '/' },
+      };
+      return paths; 
+    }
+  };
+  
