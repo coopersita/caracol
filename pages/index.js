bba80ac2e9b5edc6714d80c6303902d2a13c6dcf
Alicia Ramirez <aramirez@rrc.ca>
2021-06-22 14:39:21 -0500

    imporved CSS, added intro page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,33 @@
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  
-
   return (
     <>
-        <div className={styles.grid}>
-          <div><p className={styles.blurb}>The Mayan culture developed in Southern Mexico and Central America. It originated around 2000BC and flourished between 300AD and 900AD. The Maya are renowned for their advanced knowledge of science, Astrology and Math in particular. Today, an estimated 2 million Mayans still live in the region.</p> </div>
-          <div><Image
-          priority
-          src="/caracol/mapa_j2qnuq.svg"
-          height={1000}
-          width={1000}
-          alt='Map of the Mayan civilization encompasing southern Mexico, Guatemala, Belize, and Honduras'
-            /></div>
+      <div className={styles.grid}>
+        <div>
+          <p className={styles.blurb}>
+            The Mayan culture developed in Southern Mexico and Central America.
+            It originated around 2000BC and flourished between 300AD and 900AD.
+            The Maya are renowned for their advanced knowledge of science,
+            Astrology and Math in particular. Today, an estimated 2 million
+            Mayans still live in the region.
+          </p>
         </div>
-      
+        <div>
+          <Image
+            priority
+            src="/caracol/mapa_j2qnuq.svg"
+            height={1000}
+            width={1000}
+            alt="Map of the Mayan civilization encompasing southern Mexico, Guatemala, Belize, and Honduras"
+          />
+        </div>
+      </div>
+      <Link href="/intro">
+        <a className={styles.next}>Next</a>
+      </Link>
     </>
-  )
+  );
 }
---
+++ pages/intro.js
@@ -0,0 +1,288 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className={styles.noGrid}>
+        <div>
+          <p className={styles.blurb}>
+            The Maya developed a unique mathematical system. Unlike our decimal
+            (base 10) system, it was vigesimal (base 20).
+          </p>
+          <p className={styles.blurb}>
+            They also used the concept of zero along with an ingenious way of
+            representing numbers. With only 3 hieroglyphs, they managed to
+            represent any number:
+          </p>
+        </div>
+
+        <div className={styles.row}>
+          <figure>
+            <div className={styles.figureGroup}>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/zero_pbqspr.svg"
+                    height={107}
+                    width={254}
+                    alt="Zero hierogplyph: an oval resempling a sea shell"
+                  />
+                </div>
+              </div>
+            </div>
+            <figcaption>Zero</figcaption>
+          </figure>
+          <figure>
+            <div className={styles.figureGroup}>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+              </div>
+            </div>
+            <figcaption>One</figcaption>
+          </figure>
+          <figure>
+            <div className={styles.figureGroup}>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: an horizontal line"
+                  />
+                </div>
+              </div>
+            </div>
+            <figcaption>Five</figcaption>
+          </figure>
+        </div>
+        <div>
+          <p className={styles.blurb}>
+            So they could combine them to create other numbers:
+          </p>
+        </div>
+        <div className={styles.row}>
+          <figure>
+            <div className={styles.figureGroup}>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+              </div>
+            </div>
+            <figcaption>3</figcaption>
+          </figure>
+          <figure>
+            <div className={styles.figureGroup}>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+              </div>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: a horizontal line"
+                  />
+                </div>
+              </div>
+            </div>
+            <figcaption>9</figcaption>
+          </figure>
+          <figure>
+            <div className={styles.figureGroup}>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: a horizontal line"
+                  />
+                </div>
+              </div>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: a horizontal line"
+                  />
+                </div>
+              </div>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: a horizontal line"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <figcaption>15</figcaption>
+          </figure>
+          <figure>
+            <div className={styles.figureGroup}>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/one_nlc4nc.svg"
+                    height={31}
+                    width={31}
+                    alt="One hierogplyph: a dot"
+                  />
+                </div>
+              </div>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: a horizontal line"
+                  />
+                </div>
+              </div>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: a horizontal line"
+                  />
+                </div>
+              </div>
+              <div className={styles.figureOuter}>
+                <div className={styles.figureInner}>
+                  <Image
+                    priority
+                    src="/caracol/five_kylwqh.svg"
+                    height={16}
+                    width={254}
+                    alt="Five hierogplyph: a horizontal line"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <figcaption>19</figcaption>
+          </figure>
+        </div>
+      </div>
+      <Link href="/">
+        <a className={styles.prev}>Previous</a>
+      </Link>
+    </>
+  );
+}
