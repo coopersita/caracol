6e66236014eed732459e4f3a47a22ba19d2bf473
Alicia Ramirez <aramirez@rrc.ca>
2021-06-30 12:12:22 -0500

    Added tabs to the action page

--- pages/actions.js
+++ pages/actions.js
@@ -1,12 +1,33 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Link from 'next/link';
 import ConvertNumber from '../components/ConvertNumber.js';
 import styles from '../styles/Actions.module.css';
+import stylesIntro from '../styles/Intro.module.css';
+
+import 'react-tabs/style/react-tabs.css';
 
 export default function Actions() {
   return (
     <>
-      <div className={styles.grid}>
-        <ConvertNumber />
+      <div className={stylesIntro.noGrid}>
+        <Tabs className={styles.tabs}>
+          <TabList>
+            <Tab>Convert a number</Tab>
+            <Tab>Create a number</Tab>
+          </TabList>
+          <TabPanel>
+            <ConvertNumber />
+          </TabPanel>
+          <TabPanel>
+            <p>
+              Agnes Caroline Thaarup Obel is a Danish singer/songwriter. Her
+              first album, Philharmonics, was released by PIAS Recordings on 4
+              October 2010 in Europe. Philharmonics was certified gold in June
+              2011 by the Belgian Entertainment Association (BEA) for sales of
+              10,000 Copies.
+            </p>
+          </TabPanel>
+        </Tabs>
       </div>
       <div className="buttons">
         <Link href="/math">
