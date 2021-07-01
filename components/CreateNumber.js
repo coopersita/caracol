da65fa67fdb66805fff0c5e83fcee8fb4257ebba
Alicia Ramirez <aramirez@rrc.ca>
2021-07-01 10:30:40 -0500

    improved basis for create a number

--- components/CreateNumber.js
+++ components/CreateNumber.js
@@ -16,29 +16,37 @@ export default function CreateNumber() {
   const formatter = new Intl.NumberFormat('en-CA');
 
   return (
-    <div className={styles.grid}>
-      <div>
-        <ul className={styles.slots}>
-          {factors.map((f) => (
-            <li key={`${f}factor`}>
-              <div className={styles.slot} />
-              <div>&times; {formatter.format(f)} = </div>
+    <>
+      <p className="blurb">
+        To create a number, drag a symbol into a slot. To replace a number, drag
+        a different number on top of it.
+      </p>
+      <div className={styles.grid}>
+        <div>
+          <ul className={styles.slots}>
+            {factors.map((f, index) => (
+              <li key={`${f}factor`}>
+                <div className={styles.slot}>
+                  <GetNumberImage num={index} size={4} />
+                </div>
+                <div>&times; {formatter.format(f)} = </div>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <strong>Total</strong>: {mayanNumber}
+          </div>
+        </div>
+        <ul className={styles.gridNumbers}>
+          {numbers.map((num) => (
+            <li key={`${num}number`}>
+              <GetNumberImage numb={num} size={4} />
+              <br />
+              {num}
             </li>
           ))}
         </ul>
-        <div>
-          <strong>Total</strong>: {mayanNumber}
-        </div>
       </div>
-      <ul className={styles.gridNumbers}>
-        {numbers.map((num) => (
-          <li key={`${num}number`}>
-            <GetNumberImage numb={num} size={4} />
-            <br />
-            {num}
-          </li>
-        ))}
-      </ul>
-    </div>
+    </>
   );
 }
